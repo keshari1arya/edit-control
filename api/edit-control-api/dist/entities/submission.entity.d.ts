b22a40d31dd37b055253dca579d068104bac39aa
Arya Choudhury <achoudhary@universaltennis.com>
2023-10-08 13:16:12 +0530

    cache clear of git

--- api/edit-control-api/dist/entities/submission.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from './task.entity';
-import { Video } from './video.entity';
-export declare class Submission {
-    id: number;
-    video: Video;
-    isApproved: boolean;
-    comment: string;
-    task: Task;
-}
--- api/edit-control-api/dist/entities/taskvideo.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { Task } from './task.entity';
-import { Video } from './video.entity';
-export declare class TaskVideo {
-    id: number;
-    task: Task;
-    video: Video;
-}
